784165ae5163fd0ac68287c787b5ea19c7444bab
GASROT <guilhermedeearaujo@gmail.com>
2026-06-05 21:48:15 -0300

    refactor(project): reorganizar código para melhorar legibilidade e manutenção

--- frontend-mobile/babel.config.js
+++ frontend-mobile/babel.config.js
@@ -1,13 +1,17 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@': './src',
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
